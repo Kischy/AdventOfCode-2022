ece119185714eae70d25da99182b1d292fb31a05
Kischy <kischy@gmx.de>
2022-12-03 23:38:42 +0100

    Finished d2A

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -1,3 +1,21 @@
+pub mod rps_score_calculator;
+
+use std::env;
+use std::fs;
+
+use rps_score_calculator::rps_score_calculator::RPSCalculator;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    let input = fs::read_to_string(file_path).unwrap();
+
+    let mut rps_score_calculator = RPSCalculator::new();
+    let mut total_score = 0;
+    for line in input.lines() {
+        total_score += rps_score_calculator.calculate_score(line);
+    }
+
+    println!("Total score {}", total_score);
+
 }
--- d2/src/rps_score_calculator/rps_score_calculator.rs
+++ d2/src/rps_score_calculator/rps_score_calculator.rs
@@ -1,1 +1,93 @@
-struct RPSCalculator {}
+use std::collections::HashMap;
+
+pub struct RPSCalculator {
+    scores: HashMap<String, u128>,
+}
+
+#[derive(PartialEq, Clone, Copy)]
+enum RockPaperScissors {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn get_shape_points(rps: RockPaperScissors) -> u128 {
+    match rps {
+        RockPaperScissors::Rock => return 1,
+        RockPaperScissors::Paper => return 2,
+        RockPaperScissors::Scissors => return 3,
+    }
+}
+
+fn get_match_points(my_rps: RockPaperScissors, op_rps: RockPaperScissors) -> u128 {
+    if my_rps == op_rps {
+        return 3;
+    }
+
+    if my_rps == RockPaperScissors::Rock && op_rps == RockPaperScissors::Scissors {
+        return 6;
+    }
+
+    if my_rps == RockPaperScissors::Paper && op_rps == RockPaperScissors::Rock {
+        return 6;
+    }
+
+    if my_rps == RockPaperScissors::Scissors && op_rps == RockPaperScissors::Paper {
+        return 6;
+    }
+
+    0
+}
+
+impl RPSCalculator {
+    pub fn new() -> RPSCalculator {
+        RPSCalculator {
+            scores: HashMap::new(),
+        }
+    }
+
+    pub fn calculate_score(&mut self, game: &str) -> u128 {
+        if let Some(val) = self.scores.get(game) {
+            return *val;
+        }
+
+        let mut oponent_selection = RockPaperScissors::Paper;
+        let mut my_selection = RockPaperScissors::Paper;
+
+        for c in game.chars() {
+            if c == 'A' {
+                oponent_selection = RockPaperScissors::Rock;
+            } else if c == 'B' {
+                oponent_selection = RockPaperScissors::Paper;
+            } else if c == 'C' {
+                oponent_selection = RockPaperScissors::Scissors;
+            } else if c == 'X' {
+                my_selection = RockPaperScissors::Rock;
+            } else if c == 'Y' {
+                my_selection = RockPaperScissors::Paper;
+            } else if c == 'Z' {
+                my_selection = RockPaperScissors::Scissors;
+            }
+        }
+        let score =
+            get_shape_points(my_selection) + get_match_points(my_selection, oponent_selection);
+        self.scores.insert(game.to_string(), score);
+        score
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::rps_score_calculator::rps_score_calculator::RPSCalculator;
+
+    #[test]
+    fn get_fattest_elf_test() {
+        let mut rps_calc = RPSCalculator::new();
+        assert_eq!(rps_calc.calculate_score("A Y"), 2 + 6);
+        assert_eq!(rps_calc.calculate_score("B X"), 1 + 0);
+        assert_eq!(rps_calc.calculate_score("C Z"), 3 + 3);
+        assert_eq!(rps_calc.calculate_score("A Y"), 2 + 6);
+        assert_eq!(rps_calc.calculate_score("B X"), 1 + 0);
+        assert_eq!(rps_calc.calculate_score("C Z"), 3 + 3);
+    }
+}
